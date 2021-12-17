42748902d43128f777645f6525a3f9feac148a50
salah-bel <54447309+salah-bel@users.noreply.github.com>
2021-12-17 17:32:06 +0100

    1st form builder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { User } from './models/user.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+	nom:string = "Marie";
+
   title = 'angform';
+  users : User[] = [];
+
+  userForm: FormGroup;
+  form = new FormGroup({
+
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+
+  ngOnInit(): void {
+	console.log(this.users)
+	this.initForm();
+  }
+
+  initForm() {
+    this.userForm = this.fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      drinkPreference: '',
+	  hobbies: this.fb.array([])
+    });
+}
+
+onSubmitForm() {
+    const formValue = this.userForm.value;
+
+    const newUser = new User(
+      formValue['firstName'],
+      formValue['lastName'],
+      formValue['email'],
+      formValue['drinkPreference'],
+	  formValue['hobbies']? formValue['hobbies']:[] // si le user n'a pas de hobbie on envoie array vide
+    );
+	this.addUsers(newUser);
+
+    // this.router.navigate(['/users']);
+  }
+  	// r2CUPERER LE TABLEAU DES HOBBIES
+  getHobbies() {
+	  // récuper la propriete hobbies entant que tableau de hobbies
+	  return this.userForm.get('hobbies') as FormArray;
+  }
+
+  //AJOUTER DES CHAMPS
+  onAddHobby() {
+	  const newHobby = this.fb.control(''); // la method control permet de rajouter des champs 
+	  this.getHobbies().push(newHobby);
+  }
+
+  addUsers(user : User): void {
+	  this.users.push(user);
+	  localStorage.setItem("users", JSON.stringify(this.users)); 
+	  const userStored = localStorage.getItem("users");
+	  console.log(userStored);
+  }
+  
+  onClicked() {
+	  console.log("Clicked");
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+	  FormsModule,
+	  BrowserModule,
+    AppRoutingModule,
+	ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+	  FormBuilder
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
